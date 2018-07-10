4f203ed9856448cfa9abbdfddd5d4c4e26b3cd2e
Gary Hall <guategary@gmail.com>
2018-07-10 10:21:14 -0400

    Fixed seeds

--- db/seeds/apitokens.seeds.rb
+++ db/seeds/apitokens.seeds.rb
@@ -1,1 +1,2 @@
-AccessToken.create(token: "9931c7c14267f95303df6518c28d8fd6")
+token = AccessToken.create(token: "9931c7c14267f95303df6518c28d8fd6")
+token.challenges.build(appid: "88cd371c98dded59", name: "Star Trek Captains timed challenge", desc:"Identify the five Captains pictured", photo_name: "startrekcaptains.jpg").save
--- db/seeds/challenges.seeds.rb
+++
@@ -1,1 +0,0 @@
-Challenge.create(appid: "88cd371c98dded59", name: "Star Trek Captians timed challenge", desc:"Identify the five Captains pictured", photo_name: "startrekcaptains.jpg")
