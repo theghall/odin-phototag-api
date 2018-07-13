0a3fb71375492617884749284e33da82017a87e0
Gary Hall <guategary@gmail.com>
2018-07-13 12:02:34 -0400

    Fix coords and meta data

--- db/seeds/apitokens.seeds.rb
+++ db/seeds/apitokens.seeds.rb
@@ -1,2 +1,2 @@
 token = AccessToken.create(token: "9931c7c14267f95303df6518c28d8fd6")
-token.challenges.build(appid: "88cd371c98dded59", name: "Star Trek Captains timed challenge", desc:"Identify the five Captains pictured", photo_name: "startrekcaptains.jpg", meta_data: '{ctype: "timed"}').save
+token.challenges.build(appid: "88cd371c98dded59", name: "Star Trek Captains timed challenge", desc:"Identify the five Captains pictured", photo_name: "startrekcaptains.jpg", meta_data: '{ctype: "timed", ctime: 30000, directed: "true"}').save
--- db/seeds/photos.seeds.rb
+++ db/seeds/photos.seeds.rb
@@ -1,13 +1,13 @@
 photo = Photo.create(name: "Five Star Trek Captains", desc: "Five Star Trek Captians", difficulty: "easy" , category: "scifi", image_filename: "startrekcaptains.jpg")
 #<map name="image-map">
-#    <area target="" alt="sisko" title="sisko" href="" coords="195,3,336,204" shape="rect">
-#    <area target="" alt="archer" title="archer" href="" coords="354,0,487,196" shape="rect">
-#    <area target="" alt="picard" title="picard" href="" coords="516,3,651,203" shape="rect">
-#    <area target="" alt="kirk" title="kirk" href="" coords="679,2,822,198" shape="rect">
-#    <area target="" alt="janeway" title="janeway" href="" coords="865,4,1044,191" shape="rect">
+#    <area target="" alt="Sisko" title="Sisko" href="" coords="138,3,238,146" shape="rect">
+#    <area target="" alt="Archer" title="Archer" href="" coords="252,1,346,142" shape="rect">
+#    <area target="" alt="Picard" title="Picard" href="" coords="363,1,463,142" shape="rect">
+#    <area target="" alt="Kirk" title="Kirk" href="" coords="481,1,584,142" shape="rect">
+#    <area target="" alt="Janeway" title="Janeway" href="" coords="611,3,739,134" shape="rect">
 #</map>
-photo.items.build(name: "Sisko", top_left_loc: "195,3", bottom_right_loc: "336,204").save
-photo.items.build(name: "Archer", top_left_loc: "354,0", bottom_right_loc: "487, 196").save
-photo.items.build(name: "Picard", top_left_loc: "516,3", bottom_right_loc: "516,203").save
-photo.items.build(name: "Kirk", top_left_loc: "679,2", bottom_right_loc: "822,198").save
-photo.items.build(name: "Janeway", top_left_loc: "865,4", bottom_right_loc: "1044,191").save
+photo.items.build(name: "Sisko", top_left_loc: "138,3", bottom_right_loc: "238,146").save
+photo.items.build(name: "Archer", top_left_loc: "252,1", bottom_right_loc: "346,142").save
+photo.items.build(name: "Picard", top_left_loc: "363,1", bottom_right_loc: "463,142").save
+photo.items.build(name: "Kirk", top_left_loc: "481,1", bottom_right_loc: "584,142").save
+photo.items.build(name: "Janeway", top_left_loc: "611,3", bottom_right_loc: "739,134").save
