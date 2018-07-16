7f5f488052c9d9fc9fb76efa25dab1942434dfb1
Gary Hall <guategary@gmail.com>
2018-07-16 12:45:04 -0400

    Add new photo and rework seed structure

--- db/seeds/apitokens.seeds.rb
+++ db/seeds/apitokens.seeds.rb
@@ -1,2 +1,1 @@
 token = AccessToken.create(token: "9931c7c14267f95303df6518c28d8fd6")
-token.challenges.build(appid: "88cd371c98dded59", name: "Star Trek Captains timed challenge", desc:"Click on the face of each captain", photo_name: "startrekcaptains.jpg", meta_data: '{"ctype": "timed", "ctime": 30000, "directed": true}').save
